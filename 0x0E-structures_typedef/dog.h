fee5dc31dbef82ca809391ae4f398b2a7e7783cd
PapsJay <donallotei@gmail.com>
2022-08-01 05:39:29 -0700

    .hfile

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,32 +1,18 @@
-#ifndef DOG_H
-#define DOG_H
-
-/*
- * File: dog.h
- * Desc: Header file that defines a new type struct dog.
- */
+#include <stdio.h>
+#include "dog.h"
 
 /**
- * struct dog - A new type describing a dog.
- * @name: The name of the dog.
- * @age: The age of the dog.
- * @owner: The owner of the dog.
+ * main - check the code for ALX School students.
+ *
+ * Return: Always 0.
  */
-struct dog
+int main(void)
 {
-	char *name;
-	float age;
-	char *owner;
-};
+    struct dog my_dog;
 
-/**
- * dog_t - Typedef for struct dog
- */
-typedef struct dog dog_t;
-
-void init_dog(struct dog *d, char *name, float age, char *owner);
-void print_dog(struct dog *d);
-dog_t *new_dog(char *name, float age, char *owner);
-void free_dog(dog_t *d);
-
-#endif
+    my_dog.name = "Poppy";
+    my_dog.age = 3.5;
+    my_dog.owner = "Bob";
+    printf("My name is %s, and I am %.1f :) - Woof!\n", my_dog.name, my_dog.age);
+    return (0);
+}
