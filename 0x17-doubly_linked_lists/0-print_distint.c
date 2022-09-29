df2206a81999c7a83a13fa1aafbd93cab9387a5d
Papsjay <donallotei@gmail.com>
2022-09-29 06:33:54 -0700

    All of files

--- 0x17-doubly_linked_lists/0-print_distint.c
+++ 0x17-doubly_linked_lists/0-print_distint.c
@@ -1,29 +1,19 @@
 #include "lists.h"
-
 /**
- * print_dlistint - prints all the elements of a dlistint_t list.
- * @h: pointer to the head of the list
- *
- * Return: number of nodes
+ * print_dlistint - print all the elements of a dlistint_t list
+ * @h: pointer to the head of the DLL
+ * Return: the number of nodes type size_t
  */
-
 size_t print_dlistint(const dlistint_t *h)
 {
+	size_t num_of_nodes;
 
-	int counter = 0;
-
-	if (h == NULL)
-		return (counter);
-
-	while (h->prev != NULL)
-		h = h->prev;
-
-	while (h != NULL)
+	num_of_nodes = 0;
+	while (h)
 	{
 		printf("%d\n", h->n);
 		h = h->next;
-		counter++;
+		num_of_nodes = num_of_nodes + 1;
 	}
-
-	return (counter);
+	return (num_of_nodes);
 }
