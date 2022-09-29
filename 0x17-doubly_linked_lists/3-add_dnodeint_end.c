2953cc97e983ec342f195acd1eed8369c76ab5e1
Papsjay <donallotei@gmail.com>
2022-09-29 06:50:34 -0700

    All of files

--- 0x17-doubly_linked_lists/3-add_dnodeint_end.c
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -1,37 +1,35 @@
 #include "lists.h"
+
 /**
- * add_dnodeint_end - adds a new node at the end of a DLL
- * @head: double pointer to the beggining of the list
- * @n: content of the new node to be add
- * Return: the address of the new element or NULL if it failed
+ * add_dnodeint_end - Add a new node at the end of a `dlistint_t` list
+ * @head: double pointer to head node
+ * @n: value to store in new node
+ * Return: Address of new element, or NULL if it failed
  */
 dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 {
-	dlistint_t *last_node;
-	dlistint_t *addrs_new_node;
+	dlistint_t *tmp, *new;
 
-	addrs_new_node = malloc(sizeof(dlistint_t));
-	if (addrs_new_node == NULL)
+	new = malloc(sizeof(dlistint_t));
+	if (new == NULL)
 		return (NULL);
-	/* add data to addrs_new_node */
-	addrs_new_node->next = NULL;
-	addrs_new_node->n = n;
-	/* make last_node the head to traverse the list till the end */
-	last_node = *head;
-	if (last_node == NULL)
+
+	new->n = n;
+	new->next = NULL;
+	tmp = *head;
+	while (tmp && tmp->next)
+		tmp = tmp->next;
+
+	if (tmp)
 	{
-		/* if the list is null */
-		/* addrs_new_node becomes the head of the list*/
-		addrs_new_node->prev = NULL;
-		*head = addrs_new_node;
-		return (addrs_new_node);
+		new->prev = tmp;
+		tmp->next = new;
 	}
-	/* else, traverse till last node */
-	while (last_node->next != NULL)
-		last_node = last_node->next;
-	/* when we get the end of the list change next of last_node*/
-	/* with the new_node */
-	last_node->next = addrs_new_node;
-	addrs_new_node->prev = last_node;
-	return (addrs_new_node);
+	else
+	{
+		*head = new;
+		new->prev = NULL;
+	}
+
+	return (new);
 }
